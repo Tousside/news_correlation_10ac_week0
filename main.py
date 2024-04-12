03cf5aa5c70d72cb4717ac736ee2a638d45349c1
Tousside <122688845+Tousside@users.noreply.github.com>
2024-04-12 17:41:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def tf_idf_similarty(title, body):
+def tf_idf_similarity(title, body):
   # cobine title and body in a list
   title_body=[title, body]
 
@@ -10,11 +10,11 @@ def tf_idf_similarty(title, body):
   similarity_scores = cosine_similarity( tfidf_matrix , tfidf_matrix)
   return similarity_scores[0,1]
 
-if __name__=="__tf_idf_similarty__":
+if __name__=="__tf_idf_similarity__":
     title="superstar chef yannick alléno brings refined french cuisine pavyllon london"
     body= "avyllon london four seasons hotel london park lane one worlds best chefs\
     created new culinary stage london yannick alléno superstar french chef \
     nabbed impressive 15 michelin stars recently made uk debut opening pavyllon\
     london located four seasons hotel london park lane fashionable mayfair\
     new restaurant showcases allénos modern"
-    tf_idf_similarty(title, body)
+    tf_idf_similarity(title, body)
