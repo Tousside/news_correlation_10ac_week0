a619bdc9e05372b18f0cadfc48c6998742a36751
Tousside <122688845+Tousside@users.noreply.github.com>
2024-04-12 17:42:32 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,11 @@
-from main import tf_idf_similarty
+from main import tf_idf_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import unittest
 import numpy
 
 class TestMain(unittest.TestCase):
-    def test_tf_idf_similarty(self):
+    def test_tf_idf_similarity(self):
         title="superstar chef yannick alléno brings refined french cuisine pavyllon london"
         body= "avyllon london four seasons hotel london park lane one worlds best chefs\
     created new culinary stage london yannick alléno superstar french chef \
@@ -15,12 +15,12 @@ class TestMain(unittest.TestCase):
         self.assertIsInstance(tf_idf_similarity(title, body), numpy.float64)
         print("Test passed")
 
-if __name__== "__tf_idf_similarty__":
+if __name__== "__tf_idf_similarity__":
     title="superstar chef yannick alléno brings refined french cuisine pavyllon london"
     body= "avyllon london four seasons hotel london park lane one worlds best chefs\
     created new culinary stage london yannick alléno superstar french chef \
     nabbed impressive 15 michelin stars recently made uk debut opening pavyllon\
     london located four seasons hotel london park lane fashionable mayfair\
     new restaurant showcases allénos modern"
-    unittest.tf_idf_similarty(title, body)
+    unittest.tf_idf_similarity(title, body)
 
