9e19d75eb3c7bb85a8764dc031fef78a4566d7b0
Tousside <toussaint.amoussouvi@yahoo.com>
2024-04-12 17:20:42 +0100

    Unit test

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello, World")
-    return "Hello, World"
+def tf_idf_similarty(title, body):
+  # cobine title and body in a list
+  title_body=[title, body]
 
-if __name__=="__main__":
-    main()
+  # Create an instance of  TfidfVectorizer
+  vectorizer = TfidfVectorizer()
+  # get tf-idf scores
+  tfidf_matrix = vectorizer.fit_transform(title_body)
+  # title and body similarity score
+  similarity_scores = cosine_similarity( tfidf_matrix , tfidf_matrix)
+  return similarity_scores[0,1]
+
+if __name__=="__tf_idf_similarty__":
+    title="superstar chef yannick alléno brings refined french cuisine pavyllon london"
+    body= "avyllon london four seasons hotel london park lane one worlds best chefs\
+    created new culinary stage london yannick alléno superstar french chef \
+    nabbed impressive 15 michelin stars recently made uk debut opening pavyllon\
+    london located four seasons hotel london park lane fashionable mayfair\
+    new restaurant showcases allénos modern"
+    tf_idf_similarty(title, body)
